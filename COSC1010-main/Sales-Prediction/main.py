28c6547b42448d700a54882256793e3ab6121aa2
CapsTNT-007 <taysonwheeler@westernwyoming.edu>
2024-09-07 17:09:55 -0600

    Add files via upload

--- COSC1010-main/Sales-Prediction/main.py
+++ COSC1010-main/Sales-Prediction/main.py
@@ -1,14 +1,17 @@
 #
-# Name
-# Date
+# Name Tayson
+# Date 9/7/24
 # Sales Prediction Programming Project
 # COSC 2409 DNT
 #
 
 # Variables to hold the sales total and the profit
-
+total_sales = 0
+profit = 0
 # Get the amount of projected sales.
-
+total_sales = int(input('What was the total amout of the sales?'))
 # Calculate the projected profit.
-
+profit = total_sales * 0.23
 # Print the projected profit.
+print('The amout of profit you will earn is:')
+print(profit)
