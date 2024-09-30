ee7a504b8ba6574b4d607913e6f9df846d342e68
CapsTNT-007 <taysonwheeler@westernwyoming.edu>
2024-09-29 18:43:01 -0600

    Add files via upload

--- COSC1010-main/Feet-to-Inches/main.py
+++ COSC1010-main/Feet-to-Inches/main.py
@@ -1,7 +1,14 @@
 #
-# Name
-# Date
+# Name Tayson Wheeler
+# Date 9/29/24
 # Feet to Inches Programming Project
 # COSC 2409 DNT
 #
 # Use comments liberally throughout the program.
+
+def feet_to_inches(feet):
+    return feet * 12
+
+# Main code
+feet = int(input("How many feet would you like to convert to inches? "))
+print(feet, "feet equals", feet_to_inches(feet),"inches.")
--- COSC1010-main/Kilometer-Converter/kilometer_converter.py
+++ COSC1010-main/Kilometer-Converter/kilometer_converter.py
@@ -1,7 +1,16 @@
 #
-# Name
-# Date
+# Name Tayson Wheeler
+# Date 9/29/24
 # Kilometer Converter Programming Project
 # COSC 2409 DNT
 #
 # Use comments liberally throughout the program. 
+# Variables
+miles = 0
+kilometers = 0
+# Input
+miles = float(input("What amount of kilometers do you want to convert to miles? "))
+# Math
+kilometers = miles * 0.6214
+# Output
+print("That equates to ", (kilometers), "miles.")
