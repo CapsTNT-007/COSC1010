9fbd2ca50f2f9fc8cc90618adf67066047e95410
CapsTNT-007 <taysonwheeler@westernwyoming.edu>
2024-10-20 22:53:56 -0600

    Add files via upload

--- COSC1010-main/Average-of-Numbers/main.py
+++ COSC1010-main/Average-of-Numbers/main.py
@@ -1,7 +1,33 @@
 #
-# Name
-# Date
+# Name Tayson Wheeler
+# Date 10/20/24
 # Average of Numbers Programming Project
 # COSC 2409 DNT
 #
 # Use comments liberally throughout the program. 
+
+#Opening file
+number_file = open('numbers.txt','r')
+
+#Initializing variables
+total = 0
+count = 0
+average = 0
+
+#Getting values
+for line in number_file:
+    #convert to float
+    number = float(line)
+
+    #add to count
+    count += 1
+
+    #add to total
+    total += number
+
+#Close file
+number_file.close()
+
+#Get and display average
+average = total/count
+print(f'The aveage is {average}.')
--- COSC1010-main/File-Display/main.py
+++ COSC1010-main/File-Display/main.py
@@ -1,7 +1,18 @@
 #
-# Name
-# Date
+# Name Tayson Wheeler
+# Date 10/20/24
 # File Display Programming Project
 # COSC 2409 DNT
 #
 # Use comments liberally throughout the program. 
+
+#Open file
+files_numbers = open('numbers.txt','r')
+
+#Read file and print
+for line in files_numbers:
+    number = int(line)
+    print(number)
+
+#Close file
+files_numbers.close()
