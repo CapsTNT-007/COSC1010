99031811240278415c8da16c1f80cd7d8ecb919f
CapsTNT-007 <taysonwheeler@westernwyoming.edu>
2024-10-27 19:33:40 -0600

    Add files via upload

--- COSC1010-main/Exception-Handling/main.py
+++ COSC1010-main/Exception-Handling/main.py
@@ -1,7 +1,42 @@
 #
-# Name
-# Date
+# Name Tayson wheeler
+# Date 10/27/2024
 # Exception Handling Programming Project
 # COSC 2409 DNT
 #
 # Use comments liberally throughout the program. 
+
+#Initializing variables
+
+total = 0
+count = 0
+average = 0
+
+#Opening file + try
+try:
+    number_file = open('numbers.txt','r')
+
+    #Getting values
+    for line in number_file:
+        #convert to float
+        number = float(line)
+
+        #add to count
+        count += 1
+
+        #add to total
+        total += number
+
+    #Close file
+    number_file.close()
+
+    #Get and display average
+    average = total/count
+    print(f'The aveage is {average}.')
+
+#exceptions
+except IOError:
+    print('An error occoured trying to read the file.')
+
+except ValueError:
+    print('Non-numeric data found in the file.')
--- COSC1010-main/Lottery-Number-Generator/main.py
+++ COSC1010-main/Lottery-Number-Generator/main.py
@@ -1,7 +1,22 @@
 #
-# Name
-# Date
+# Name Tayson Wheeler
+# Date 10/27/1014
 # Lottery Number Generator Programming Project
 # COSC 2409 DNT
 #
 # Use comments liberally throughout the program. 
+
+#import random modual
+import random
+
+#create list
+lotteryNumber = [0] * 7
+
+#assigning random values
+for index in range (7):
+    lotteryNumber[index] = random.randint(0,9)
+
+#printing values
+print("Here is you lottery numbers: ")
+for index in range (7):
+    print(lotteryNumber[index], end='')
