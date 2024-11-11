28f5836112e2cfb31b1e5ad4e9f4d9ea4cc4c9ec
CapsTNT-007 <taysonwheeler@westernwyoming.edu>
2024-11-10 22:55:28 -0700

    Add files via upload

--- COSC1010-main/Pig-Latin/main.py
+++ COSC1010-main/Pig-Latin/main.py
@@ -1,7 +1,30 @@
 #
-# Name
-# Date
+# Name Tayson Wheeler
+# Date 11/10/24
 # Pig Latin Programming Project
 # COSC 1010
 #
 # Use comments liberally throughout the program. 
+
+#Get sentance and define variables
+userSentance = input("What sentance would you like to convert to Pig Latin? ")
+index = 0
+
+#Split sentance into words
+wordList = userSentance.split()
+
+#Define a function that converts each word
+def makePigLatin(currentWord):
+    newWordStart = currentWord[1:]
+    newWordEnd = currentWord[0]
+    finalWord = newWordStart + newWordEnd + "ay"
+    return(finalWord)
+
+#Uses the 'makePigLatin' function on each word to convert it
+for word in wordList:
+    wordList[index] = makePigLatin(wordList[index])
+    index += 1
+
+#joins the end words into a string and prints
+pigLatin = ' '.join(wordList)
+print(pigLatin)
--- COSC1010-main/Vowels-and-Consonants/main.py
+++ COSC1010-main/Vowels-and-Consonants/main.py
@@ -1,7 +1,38 @@
 #
-# Name
-# Date
+# Name Tayson Wheeler
+# Date 11/10/24
 # Vowels and Consonants Programming Project
 # COSC 2409 DNT
 #
 # Use comments liberally throughout the program. 
+
+#Defining funtions
+def numberVowels(word):
+    #Defining vowels and variables
+    vowles = ['a','e','i','o','u']
+    vowelCount = 0
+
+    #Checking each charecter for if its a vowel
+    for charecter in word:
+        if charecter.lower() in vowles:
+            vowelCount += 1
+    
+    #Returing numeric amount
+    return(vowelCount)
+
+def numberConstants(word):
+    #Defining vowels and variables
+    vowels = ['a','e','i','o','u']
+    charecterCount = 0
+
+    #Checking each charecter for if its a vowel
+    for charecter in word:
+        if charecter.lower() not in vowels:
+            charecterCount += 1
+
+    #Returing numeric amount
+    return(charecterCount)
+
+#Getting word and printing information
+myWord = input("Enter a string of charecters")
+print("The sting has", numberVowels(myWord), "vowels, and", numberConstants(myWord), "constants.")
